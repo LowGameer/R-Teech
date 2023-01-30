3a9927541f644e74ca967dbbef443b5ca9ed8489
LowGameer <93059023+LowGameer@users.noreply.github.com>
2023-01-30 01:28:31 -0300

    Atualizando interface

--- site/src/componentes/pages/home/Home.js
+++ site/src/componentes/pages/home/Home.js
@@ -11,19 +11,9 @@ function Home (){
     
       <div class="imgg"> <Link to="/produtos"><button class="botao" >COMPRE AGORA!</button></Link></div>
       <div>
-      <button >Compre agora! coeee</button>
-      <h1>Sobre Nós</h1>
-      <p>bla bla bla bla</p>
-      <h1>Por que nos escolher?</h1>
-      <img src={office} ></img>
-      <div>
-        <p></p>
-        <p></p>
-        <p></p>
+      <h1 class="nos">Sobre Nós</h1>
+      <p class="texto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>
-      </div>
-k
-    
     </div>
     )
 }
